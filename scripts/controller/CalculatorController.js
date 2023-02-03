c3390a911fc2bb6cf20442abdd17e91f0cb240e0
Vanderson <fcovandersonma@gmail.com>
2023-02-03 11:55:14 -0300

    Inclusao de eventos apos a interacao com botoes da calculadora

--- scripts/controller/CalculatorController.js
+++ scripts/controller/CalculatorController.js
@@ -6,6 +6,7 @@ class CalculatorController {
         this._dateEl = document.querySelector("#data");
         this._timeEl = document.querySelector("#hora");
         this.initialize();
+        this.initButtonsEvents();
     }
 
     initialize(){
@@ -16,6 +17,30 @@ class CalculatorController {
             this.setdisplayDateTime();
        }, 1000);
     }
+
+    addEventListenerAll(element, events, fn){
+        events.split(' ').forEach(event => {
+            element.addEventListener(event, fn, false);
+        });
+    }
+
+    initButtonsEvents(){
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+
+        buttons.forEach((btn, index) => {
+            
+            this.addEventListenerAll(btn, 'click drag', e => {
+                console.log(btn.className.baseVal.replace("btn-", ""));
+            });
+
+
+            this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e => {
+                btn.style.cursor = "pointer";
+            });
+        });
+    }
+
+
 
     setdisplayDateTime(){
        this.displayDate = this.currentDate.toLocaleDateString(this._locale, {day: "2-digit", month: "long", year: "numeric"});
